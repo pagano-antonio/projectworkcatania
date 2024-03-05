cc1f950c7921414345c410d28a116cee914e02e0
VeaLaErre <valelaerre@gmail.com>
2024-03-05 13:51:52 +0100

    cambio Attributo data Nascita come Date e nuova Rep

---
+++ ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateSkillRepository.java
@@ -0,0 +1,9 @@
+package com.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.CandidateSkill;
+
+public interface CandidateSkillRepository extends JpaRepository<CandidateSkill, Integer> {
+
+}
