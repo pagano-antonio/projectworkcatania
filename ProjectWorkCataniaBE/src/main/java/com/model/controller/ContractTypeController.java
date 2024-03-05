fc4ec380792c627c7ae91bada6b6015a35926bc9
JasmineDeyr <jasminedeyr@gmail.com>
2024-03-05 16:25:47 +0100

    i controller

--- ProjectWorkCataniaBE/src/main/java/com/model/controller/ContractTypeController.java
+++ ProjectWorkCataniaBE/src/main/java/com/model/controller/ContractTypeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("CandidateController")
-public class CandidateController {
+@RequestMapping("ContractTypeController")
+public class ContractTypeController {
 
 }
