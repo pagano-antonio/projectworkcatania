f3542b7133312388af46d38fe291b7bd9413b6ae
LadyTech28 <user@sara.brambi.1990@gmail.com>
2024-03-05 16:41:11 +0100

    controller update 5 marzo 16:41

---
+++ ProjectWorkCataniaBE/src/main/java/com/model/controller/CompanyClientController.java
@@ -0,0 +1,11 @@
+package com.model.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("CompanyClientController")
+public class CompanyClientController {
+	
+}
+
