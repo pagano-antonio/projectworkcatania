e711c33f0af39ce732d27680ad29a5718efc00bf
VeaLaErre <valelaerre@gmail.com>
2024-03-05 16:22:55 +0100

    Primo metodo crud addCandidate

--- ProjectWorkCataniaBE/src/main/java/com/model/rest/CandidateRest.java
+++ ProjectWorkCataniaBE/src/main/java/com/model/rest/CandidateRest.java
@@ -1,8 +1,38 @@
 package com.model.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.model.Candidate;
+import com.model.repository.CandidateRepository;
+
 @RestController
+@RequestMapping("CandidateRest")
 public class CandidateRest {
+	
+	@Autowired
+	CandidateRepository candidateRep;
+	
+	@GetMapping("/home")
+    public String goToHome(Model model) {
+        return "home";  
+    }
+	
+	@GetMapping("/preAddCandidate")
+    public String insertCandidate(Model model) {
+        return "addCandidate";
+	} 
+	
+	@GetMapping("/addCandidate")
+    public String addCandidate(Candidate candidate, Model model) {
+		System.out.println("Sto inserendo una nuovo candidato!");
+		candidateRep.save(candidate);	
+        return "addCandidateOk";
+    }
+
 
 }
