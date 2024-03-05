e0f50d13b602fe7e9cc5526bb76cc502f8e6de95
VeaLaErre <valelaerre@gmail.com>
2024-03-05 12:55:53 +0100

    Due repository

---
+++ ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateCommercialDataRepository.java
@@ -0,0 +1,9 @@
+package com.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.CandidateCommercialData;
+
+public interface CandidateCommercialDataRepository extends JpaRepository<CandidateCommercialData, Integer> {
+
+}
