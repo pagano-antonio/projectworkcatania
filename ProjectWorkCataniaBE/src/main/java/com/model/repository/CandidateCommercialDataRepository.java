54292a9bf83770998a05bc5d09c8e9c9580dae90
VeaLaErre <valelaerre@gmail.com>
2024-03-05 14:12:05 +0100

    Aggiunta Annotation @Repository che avevo dimenticato

--- ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateCommercialDataRepository.java
+++ ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateCommercialDataRepository.java
@@ -1,9 +1,11 @@
 package com.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.model.CandidateCommercialData;
 
+@Repository
 public interface CandidateCommercialDataRepository extends JpaRepository<CandidateCommercialData, Integer> {
 
 }
--- ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateSkillRepository.java
+++ ProjectWorkCataniaBE/src/main/java/com/model/repository/CandidateSkillRepository.java
@@ -1,9 +1,11 @@
 package com.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.model.CandidateSkill;
 
+@Repository
 public interface CandidateSkillRepository extends JpaRepository<CandidateSkill, Integer> {
 
 }
