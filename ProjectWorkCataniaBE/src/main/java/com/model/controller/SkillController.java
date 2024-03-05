fbd7c4a815dee433dd68407ba47817a9221af66c
LadyTech28 <user@sara.brambi.1990@gmail.com>
2024-03-05 16:52:31 +0100

    controller update 5 marzo 16:52

---
+++ ProjectWorkCataniaBE/src/main/java/com/model/controller/SkillController.java
@@ -0,0 +1,10 @@
+package com.model.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("SkillController")
+public class SkillController {
+
+}
