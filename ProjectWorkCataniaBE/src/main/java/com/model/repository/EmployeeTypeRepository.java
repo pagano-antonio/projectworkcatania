e514503098874e2bcb62c06bc298607dc390ec77
VeaLaErre <valelaerre@gmail.com>
2024-03-05 14:59:37 +0100

    Modifica del EmployeeTypeRepository

--- ProjectWorkCataniaBE/src/main/java/com/model/repository/EmployeeTypeRepository.java
+++ ProjectWorkCataniaBE/src/main/java/com/model/repository/EmployeeTypeRepository.java
@@ -3,8 +3,10 @@ package com.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.model.EmployeeType;
+
 
 @Repository
-public interface EmployeeType extends JpaRepository <EmployeeType, Integer>{
+public interface EmployeeTypeRepository extends JpaRepository <EmployeeType, Integer>{
 
 }
