5684337674071f26a95ec978665de65e7f1d7ef3
VeaLaErre <valelaerre@gmail.com>
2024-03-13 13:19:55 +0100

    "sistemate altre Jsp"

--- ProjectWorkCataniaBE/src/main/java/com/controller/JobInterviewController.java
+++ ProjectWorkCataniaBE/src/main/java/com/controller/JobInterviewController.java
@@ -9,9 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.model.Candidate;
+import com.model.Employee;
 import com.model.JobInterview;
+import com.model.StateJobInterview;
+import com.repository.CandidateRepository;
 import com.repository.EmployeeRepository;
 import com.repository.JobInterviewRepository;
+import com.repository.StateJobInterviewRepository;
 
 @Controller
 @RequestMapping("JobInterviewController")
@@ -24,8 +29,20 @@ public class JobInterviewController {
 	@Autowired
 	EmployeeRepository employeeRep;
 	
+	@Autowired
+	CandidateRepository candidateRep;
+	
+	@Autowired
+	StateJobInterviewRepository stateJobRep;
+	
 	@GetMapping("/preAddJobInterview")
     public String insertJobInterview(Model model) {
+		List<Candidate> candidate = candidateRep.findAll();
+		List<StateJobInterview> stateJob = stateJobRep.findAll();
+		List<Employee> employee = employeeRep.findAll();
+		model.addAttribute("candidate", candidate);
+		model.addAttribute("stateJob", stateJob);
+		model.addAttribute("employee", employee);
         return "addJobInterview";
 	} 
 	
